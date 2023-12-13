20ce9fda55bd8b84ae4699a3c9f941c7a08adad2
テイウンサツ <chengyunce@gmail.com>
2023-12-13 11:23:02 +0900

    1

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
-//Express の作成
+// Express の作成
 const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-//public フォルダーを、web公開
+// publicフォルダを、Web公開
 app.use(express.static(__dirname + '/public'));
 
-//httpのサーバーを作成
+// HTTPサーバの作成
 const { createServer } = require('node:http');
 const server = createServer(app);
 
 //環境変数読み込み
 const dotenv = require('dotenv');
 dotenv.config();
-const host = process.env.HOST
-const port = process.env.PORT
+const host = process.env.HOST;
+const port = process.env.PORT;
 
-//httpサーバーを待機
-server.listen(port, host, () => {
+//Socket.ioで通信
+//Socket.ioモジュール読み込み
+const { Server } = require('socket.io');
+//Socket.io作成
+const io = new Server(server);
+
+// connection event
+io.on('connection', (socket) => {
+    console.log('connected!!!');
+
+    //chatメッセージの受信
+    socket.on('chat_message', (data) => {
+        console.log(socket.id);
+        console.log(data);
+        //送信ユーザのSocketIDを追加
+        data.socket_id = socket.id;
+        //接続しているユーザにメッセージを送信
+        io.emit('chat_message', data);
+    });
+});
+
+// HTTPサーバー待機
+server.listen(port, host, ()=> {
+    console.log('Server Listen...');
     console.log(`listening on http://${host}:${port}`);
-    })
+})
