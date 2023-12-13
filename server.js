bfd129bf7740f736192b780583c5589e619bbfb3
テイウンサツ <chengyunce@gmail.com>
2023-12-13 10:36:44 +0900

    1

--- server.js
+++ server.js
@@ -5,3 +5,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //public フォルダーを、web公開
 app.use(express.static(__dirname + '/public'));
+
+//httpのサーバーを作成
+const { createServer } = require('node:http');
+const server = createServer(app);
+
+//環境変数読み込み
+const dotenv = require('dotenv');
+dotenv.config();
+const host = process.env.HOST
+const port = process.env.PORT
+
+//httpサーバーを待機
+server.listen(port, host, () => {
+    console.log(`listening on http://${host}:${port}`);
+    })
